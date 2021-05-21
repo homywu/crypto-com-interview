2b00cdeab56a81b80984739ee59ba981906d1dae
Samson Wu <samson.woooo@gmail.com>
2021-05-21 15:44:09 +0800

    update README for tests

--- src/libs/mnemonic.test.ts
+++ src/libs/mnemonic.test.ts
@@ -1,5 +1,3 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
 import { Mnemonic } from './mnemonic';
 import { randomBytes } from 'crypto';
 import { mnemonicFixtures } from '../test/mnemonic.fixtures';
