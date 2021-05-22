fec0e673d3fcacf025095042d0a09b3c5acf72c8
Samson Wu <samson.woooo@gmail.com>
2021-05-22 11:33:34 +0800

    add more tests for mnemonic

--- src/libs/mnemonic.test.ts
+++ src/libs/mnemonic.test.ts
@@ -1,6 +1,7 @@
 import { Mnemonic } from './mnemonic';
 import { randomBytes } from 'crypto';
-import { mnemonicFixtures } from '../test/mnemonic.fixtures';
+import { mnemonicFixtures } from '../test/fixtures/mnemonic.fixtures';
+import { fromBase58, fromSeed } from 'bip32';
 
 export const getRandomValues = (array: Uint8Array) => {
   if (!(array instanceof Uint8Array)) {
@@ -28,10 +29,46 @@ describe('Mnemonic', () => {
         expect(phrase.split(' ').length).toEqual(fixture.numbOfWords);
       });
 
-      it('should equal to the expected phrase', () => {
+      it(`should parse the random value ${fixture.randomValue}, equal to the expected phrase`, () => {
         const phrase = Mnemonic.toMnemonic(fixture.randomValue);
         expect(phrase).toEqual(fixture.phrase);
       });
     }
   });
+
+  describe('toSeed', () => {
+    for (const fixture of mnemonicFixtures) {
+      it(`can convert phrase ${fixture.phrase} to seed, and equal to expected restored seed`, () => {
+        const seed = Mnemonic.toSeed(fixture.phrase);
+        const rootKey = fromSeed(seed);
+        const restored = fromBase58(fixture.rootKey);
+
+        expect(rootKey.toWIF()).toEqual(restored.toWIF());
+      });
+    }
+  });
+
+  describe('toBip32RootKey', () => {
+    for (const fixture of mnemonicFixtures) {
+      it(`can convert phrase ${fixture.phrase} to root key, and equal to expected root key`, () => {
+        const seed = Mnemonic.toSeed(fixture.phrase);
+        const rootKey = Mnemonic.toBip32RootKey(seed);
+
+        expect(rootKey.toBase58()).toEqual(fixture.rootKey);
+      });
+    }
+  });
+
+  describe('getDerivationPath', () => {
+    it('should return the expectd derivation path', () => {
+      expect(Mnemonic.getDerivationPath(32, 1, 0, 0)).toEqual('m/32\'/1\'/0\'/0');
+    });
+  });
+
+  describe('getDerivationPath', () => {
+    it('should return the expectd derivation path', () => {
+      expect(Mnemonic.getBip44DerivationPath(32, 1, 0)).toEqual('m/32\'/1\'/0\'');
+    });
+  });
+
 });
